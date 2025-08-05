2e8b00776e847876b2f936de092164d77c9d9e6b
sdmangrule <137779828+sdmangrule@users.noreply.github.com>
2025-08-05 11:44:18 +0530

    Create hash.pl
    hash operations

---
+++ hash.pl
@@ -0,0 +1,44 @@
+#!/usr/bin/perl
+
+use strict;
+use warnings;
+
+my %person = (
+        "name" => 'sachin',
+        "age"  => 36,
+        "city" => 'Pune'
+);
+
+print "\n Person NAme == $person{'name'}";
+print "\n Person age == $person{'age'}";
+print "\n Person city == $person{'city'}";
+
+$person{"job"} = "Engineer";
+
+print "\n Person job == $person{'job'}" if exists $person{"job"};
+
+delete ($person{"job"});
+
+print "\n Person job == $person{'job'}";
+
+
+print "\n scalar/count of KEYS == ". keys(%person);
+
+while (my ($key, $value) = each (%person)){
+
+    print "\n  $key == $value  \n";
+}
+
+=cut
+## COMMENT the use strict pragma to test below code ####
+my %hash = ("Geeks",of, "Geek");
+
+# each() function for scalar context
+while (my ($key) = each(%hash))
+{
+    
+    # Printing(key)
+    print("$key == $hash{$key} \n");
+}
+=cut
+
